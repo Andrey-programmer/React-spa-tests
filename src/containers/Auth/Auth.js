7d4cf14315d2ec6f3b036d4c353ef2bdf4669191
Andrey_programmer <andrey@example.com>
2018-12-12 15:41:03 +0300

    Block_9 - Lesson_1 - Наброшена страница авторизации

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import classes from './Auth.css'
+import Button from '../../compionents/UI/Button/Button'
 
 
 class Auth extends Component {
@@ -6,11 +8,46 @@ class Auth extends Component {
         super(props);
         this.state = {  }
     }
+
+
+    autorization = () => {
+
+    }
+
+    registration = () => {
+
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+    }
+    
     render() { 
         return ( 
-            <div>
-                Auth
-            </div>
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Авторизация</h1>
+                    <form 
+                        onSubmit={this.submitHandler}
+                        className={classes.AuthForm}
+                    >
+                        <input type="text"/>
+                        <input type="text"/>
+                        <Button 
+                            type="success" 
+                            onClick={this.autorization}
+                        >
+                            Войти в систему
+                        </Button>
+                        <Button 
+                            type="primary"
+                            onClick={this.registration}
+                        >
+                            Зарегистрироваться 
+                        </Button>
+                    </form>
+                </div>
+            </div> 
         )
     }
 }
