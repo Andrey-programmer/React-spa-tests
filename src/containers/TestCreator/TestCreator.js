1d2c3649589d84864516bd5edf56d246b95c6d9b
Andrey_programmer <andrey@example.com>
2018-12-13 18:42:50 +0300

    Накидана страница создания теста

--- src/containers/TestCreator/TestCreator.js
+++ src/containers/TestCreator/TestCreator.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import classes from './TestCreator.css'
+import Button from '../../compionents/UI/Button/Button'
+import Input from '../../compionents/UI/Input/Input'
 
 
 class TestCreator extends Component {
@@ -6,10 +9,50 @@ class TestCreator extends Component {
         super(props);
         this.state = {  }
     }
+
+    addQuestion = () => {
+
+    }
+
+    createTest = () => {
+
+    }
+ 
+    onSubmitHandler = (event) => {
+        event.preventDefault()
+    }
+
     render() { 
         return ( 
-            <div>
-                TestCreator
+            <div className={classes.TestCreator}>
+                <div>
+                    <h1>Создание теста</h1>
+                    <form
+                        onSubmit = {this.onSubmitHandler}
+                    >
+                        <Input/>
+                        <hr/>
+                        <Input/>
+                        <Input/>
+                        <Input/>
+                        <Input/>
+
+                        <select></select>
+                        <Button
+                            type="primary"
+                            onClick={this.addQuestion}
+                        >
+                            Добавить вопрос
+                        </Button>
+                        <Button
+                            type="success"
+                            onClick={this.createTest}
+                        >
+                            Создать тест
+                        </Button>
+                    
+                    </form>
+                </div>
             </div>
         );
     }
