8255acfffe1197e2a1af7cae2791ecc382d2fcbb
Andrey_programmer <andrey@example.com>
2018-12-17 13:42:13 +0300

    Добавлен лоадер на страницу списка тестов

--- src/containers/TestList/TestList.js
+++ src/containers/TestList/TestList.js
@@ -1,26 +1,57 @@
 import React, { Component } from 'react'
 import classes from './TestList.css'
 import {NavLink} from 'react-router-dom'
+import Loader from '../../compionents/UI/Loader/Loader'
+import Axios from 'axios';
 
 class TestList extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            tests: [],
+            loading: true
+         }
     }
 
     renderTests() {
-        return [1, 2, 3].map((test, index) => {
+        return this.state.tests.map((test) => {
             return (
                 <li 
-                    key={index}
+                    key={test.id}
                 >
-                    <NavLink to={'/test/' + test}>
-                        Тест {test}
+                    <NavLink to={'/test/' + test.id}>
+                        {test.name}
                     </NavLink> 
-                </li>
+                </li> 
             )
         })
     }
+
+    async componentDidMount() {
+
+        try {
+            const tests = []
+
+            const response = await Axios.get('https://react-spa-tests.firebaseio.com/tests.json')
+
+
+
+
+            Object.keys(response.data).forEach((id, index) => {
+                tests.push({
+                    id,
+                    name: `Тест №${index + 1}`
+                })
+            })
+            console.log(response.data)
+
+            this.setState({tests, loading: false})
+
+        } catch(error) {
+            console.log(error)
+        }
+    
+    } 
 
     render() { 
         return ( 
@@ -28,11 +59,16 @@ class TestList extends Component {
                 <div>
                     <h1>Список тестов</h1>
 
-                    <ul>
-                        {this.renderTests()}
-                    </ul>
+                    {this.state.loading 
+                        ? <Loader/> 
+                        :   <ul>
+                                {this.renderTests()}
+                            </ul>
+                    }
+                   
                 </div>
             </div>
+
          );
     }
 }
