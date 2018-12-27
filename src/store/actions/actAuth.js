f40e055495d9a148db34b45ff2df2ce8d1b53dbc
Andrey_programmer <andrey@example.com>
2018-12-27 13:41:41 +0300

    Получаем код ошибки при авторизации в actAuth

--- src/store/actions/actAuth.js
+++ src/store/actions/actAuth.js
@@ -15,17 +15,24 @@ export function auth(email, password, isLogin) {
             url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDZpCwjjFjSbYcji0dgeDvEeEY6iIsLWPY'
         }
 
-        const response = await axios.post(url, authData)
-        const data = response.data
-
-        const expirationDate = new Date(new Date().getTime() + data.expiresIn * 1000)
-
-        localStorage.setItem('token', data.idToken)
-        localStorage.setItem('userId', data.localId)
-        localStorage.setItem('expirationDate', expirationDate)
-
-        dispatch(authSuccess(data.idToken))
-        dispatch(autoLogout(data.expiresIn))
+        try {
+            const response = await axios.post(url, authData)
+            const data = response.data
+    
+            const expirationDate = new Date(new Date().getTime() + data.expiresIn * 1000)
+    
+            localStorage.setItem('token', data.idToken)
+            localStorage.setItem('userId', data.localId)
+            localStorage.setItem('expirationDate', expirationDate)
+    
+            dispatch(authSuccess(data.idToken))
+            dispatch(autoLogout(data.expiresIn))
+        } catch(error) {
+            console.log('error', JSON.stringify(error))
+            console.log('error', error.response.data.error.code)
+            throw error
+        }
+       
     }
 }
 
